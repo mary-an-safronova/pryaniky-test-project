43122e0203944e06143d58e1c65add62550e134b
Mariia Safronova <mary.an.safronova@yandex.ru>
2024-09-05 11:10:55 +0300

    fix: SignInForm is placed in a separate component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
+export { SignInForm as SignInForm } from './sign-in-form';
 export { DocumentTable as DocumentTable } from './table';
 export { ProtectedRouteElement as ProtectedRouteElement } from './protected-route-element';
